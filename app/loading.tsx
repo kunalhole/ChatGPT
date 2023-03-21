2e750206716f24b5a8664452a8ef747f9c8cdadc
Kunal Hole <kunalhole538@gmail.com>
2023-03-21 11:07:09 +0530

    Bug Fixing

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,8 +1,8 @@
 const loading = () => {
   return (
-    <div className="bg-blue-400 h-screen flex justify-center items-center">
-      <h1 className="text-3xl text-white font-bold animate-bounce">
-        Loding...
+    <div className="bg-[#202123] h-screen flex justify-center items-center">
+      <h1 className="text-3xl text-white font-bold animate-pulse">
+        Loading...
       </h1>
     </div>
   )
